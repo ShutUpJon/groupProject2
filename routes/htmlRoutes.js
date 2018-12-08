f9f17b2cc32d1686bb2953207b1fea6ffdd1d337
gregvx <32096447+gregvx@users.noreply.github.com>
2018-12-08 13:06:24 -0700

    Made changes to html and api routes folder.

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -3,21 +3,23 @@ var db = require("../models");
 module.exports = function(app) {
   // Load index page
   app.get("/", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
+    db.characters.findAll({}).then(function(dbCharacters) {
       res.render("index", {
         msg: "Welcome!",
-        examples: dbExamples
+        characters: dbCharacters
       });
     });
   });
 
-  // Load example page and pass in an example by id
-  app.get("/example/:id", function(req, res) {
-    db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.render("example", {
-        example: dbExample
+  // Load example page and pass in an character by id
+  app.get("/character/:id", function(req, res) {
+    db.characters
+      .findOne({ where: { id: req.params.id } })
+      .then(function(dbCharacters) {
+        res.render("character", {
+          character: dbCharacters
+        });
       });
-    });
   });
 
   // Render 404 page for any unmatched routes
@@ -25,5 +27,3 @@ module.exports = function(app) {
     res.render("404");
   });
 };
-
-
